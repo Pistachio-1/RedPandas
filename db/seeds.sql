1834a70fd9696c0e757ed34ce87228d64f982c18
Michelle Shin <mshin017@ucr.edu>
2018-06-25 19:48:27 -0700

    adding sequelize and mysql2

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO users ("user_name", "email", "user_password");
+INSERT INTO users (user_name, email, user_password);
 
-INSERT INTO rooms ("room_id","room_name");
+INSERT INTO rooms (room_id,room_name);
