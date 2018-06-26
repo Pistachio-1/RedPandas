ac1ac150cfb19cf651689ed27620d53e0e4fd851
Mike West <mister270@gmail.com>
2018-06-25 21:48:40 -0700

    initial running version of server.js and support files

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
--
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
