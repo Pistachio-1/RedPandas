06a01513ea67f71deab8aa66c2ebfe7239a14f85
Michelle Shin <mshin017@ucr.edu>
2018-06-26 23:08:38 -0700

    pseudo coded index and room js files

--- public/js/html.js
+++ public/js/html.js
@@ -17,24 +17,14 @@ $(function() {
 	});
 
 	const modal = document.getElementById('myModal');
-	
-	// Get the button that opens the modal
 	const login = document.getElementById("login-start");
-	
-	// Get the <span> element that closes the modal
 	const span = document.getElementsByClassName("close")[0];
-	
-	// When the user clicks on the button, open the modal 
 	login.onclick = function() {
 		modal.style.display = "block";
 	}
-	
-	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
 		modal.style.display = "none";
 	}
-	
-	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = "none";
@@ -43,18 +33,16 @@ $(function() {
 });
 
 $("#login-submit").on("click",function(){
-	let username = $("#username").val().trim();
+	const username = $("#username").val().trim();
+	const password = $("#password").val().trim();
 	const getLogin = (username) => {
-		if(username){
-			username = "/username/" + username;
-		}
-		$.get("/api/users"+ username, function(data){
-			console.log("Welcome" + username);
+		$.get("/api/users/"+username, function(data){
+			console.log("Welcome ", data)
 
 		})
 	}
 	// if (username = password from mysql){
-	// 	$("#welcomeUser").append("Welcome, " + username);
+	// 	$("#incorrectUserPass").append("Welcome, " + username);
 	// }
 	// else {
 	// 	$("#incorrectUserPass").append("Username or Password is incorrect. Please try again")
@@ -67,9 +55,9 @@ $("#register-submit").on("click", function(){
 	// 	post new username into database 
 	// }
 	// else if (username is taken) {
-	// 	$("#noMatch").append("this username is already taken")
+	// 	$("#tryAgain").append("this username is already taken")
 	// }
 	// else if( password !== confirm password) {
-	// 	$("#noMatch").append("passwords do not match")
+	// 	$("#tryAgain").append("passwords do not match")
 	// }
 });
---
+++ public/js/room.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    function getRooms() {
+//         $.get("/api/rooms/", function(data) {
+//         var rowsToAdd = [];
+//         for (var i = 0; i < data.length; i++) {
+//             rowsToAdd.push(createRoomRow(data[i]));
+//         }
+//         renderRoomList(rowsToAdd);
+//         });
+//   }
+})
