59a2e0008d6f266c981bd3f639161d55afd10c22
Mike West <mister270@gmail.com>
2018-06-27 20:17:38 -0700

    added more unit tests

--- js/app.unit.test.js
+++ js/app.unit.test.js
@@ -3,40 +3,57 @@ jest.autoMockOff();
 const GETMATCH = require("./app.js");
 console.log(GETMATCH);
 
+// the following piece of code will handled "unhandledRejection" events
+// which can occur when we use jest for testing. The events don't stop the 
+// tests from completing successfully but they leave nasty warnings in the test code.
+process.on('unhandledRejection', error => {
+    // Will print "unhandledRejection err is not defined"
+    console.log('unhandledRejection', error.message);
+});
+
 describe("getMatch", () => {
-    let callBack = function(results) {
+    let results = "";
+    function callback(results) {
         console.log(results);
     };
 
     it("calls getMatch with ALLMATCHES parameter",() => {
-        expect(GETMATCH.getMatch(GETMATCH.ALLMATCHES, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.ALLMATCHES, null, callback(results))).toBeDefined();
     });
 
     it("calls getMatch with CURRENTMATCHES parameter",() => {
-        expect(GETMATCH.getMatch(GETMATCH.CURRENTMATCHES, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.CURRENTMATCHES, null, callback(results))).toBeDefined();
     });
 
     it("calls getMatch with COUNTRYMATCHES and fifa_code parameter",() => {
-        expect(GETMATCH.getMatch(GETMATCH.COUNTRYMATCHES, "RUS", this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.COUNTRYMATCHES, "RUS", callback(results))).toBeDefined();
     });
 
-    // it("calls getMatch with COUNTRYMATCHES",() => {
-    //     expect(GETMATCH.getMatch(GETMATCH.COUNTRYMATCHES, null, this.callback)).toThrowError();
-    // });
+    it("calls getMatch with COUNTRYMATCHES",() => {
+        expect(GETMATCH.getMatch(GETMATCH.COUNTRYMATCHES, null, callback(results))).toThrowError();
+    });
 
     it("calls getMatch with TODAYMATCHES",() => {
-        expect(GETMATCH.getMatch(GETMATCH.TODAYMATCHES, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.TODAYMATCHES, null, callback(results))).toBeDefined();
     });
 
     it("calls getMatch with ALLRESULTS",() => {
-        expect(GETMATCH.getMatch(GETMATCH.ALLRESULTS, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.ALLRESULTS, null, callback(results))).toBeDefined();
     });
 
     it("calls getMatch with RESULTSBYGROUP",() => {
-        expect(GETMATCH.getMatch(GETMATCH.RESULTSBYGROUP, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.RESULTSBYGROUP, null, callback(results))).toBeDefined();
     });
 
     it("calls getMatch with ALLTEAMS",() => {
-        expect(GETMATCH.getMatch(GETMATCH.ALLTEAMS, null, this.callback)).toBeDefined();
+        expect(GETMATCH.getMatch(GETMATCH.ALLTEAMS, null, callback(results))).toBeDefined();
+    });
+
+    it("calls getMatch with invalid match type", () => {
+        expect(GETMATCH.getMatch("invalid", null, callback(results))).toThrowError();
+    });
+
+    it("calls getMatch with null type", () => {
+        expect(GETMATCH.getMatch(null, null, callback(results))).toThrowError();
     });
 });
