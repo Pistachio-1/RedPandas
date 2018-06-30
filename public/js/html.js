d9c3e65fd6b53c86536dd320a5f124fdc39bd10d
Mike West <mike.wesley.west@gmail.com>
2018-06-30 09:31:24 -0700

    add tomorrow's matches

--- public/js/html.js
+++ public/js/html.js
@@ -1,6 +1,7 @@
 $(function () {
 	function matchesCallback(results, html_dest) {
 		if (results.length > 0) {
+			let status = "";
 			results.forEach(r => {
 				let away = r.away_team.country;
 				let away_goals = r.away_team.goals;
@@ -18,7 +19,12 @@ $(function () {
 						break;
 				};
 
-				let matchStatus = away + " (" + away_goals + ") vs. (" + home_goals + ") " + home;
+				let matchStatus = "";
+				if (status === "PENDING") {
+					matchStatus = away + " VS. " + " " + home;
+				} else {
+					matchStatus = away + " (" + away_goals + ") vs. (" + home_goals + ") " + home;
+				}
 
 				// add country flags
 				let imgAway = document.createElement("img");
@@ -44,22 +50,9 @@ $(function () {
 
 	getMatch(TOMORROWMATCHES, null)
 		.then(results => matchesCallback(results, $("#tomorrows-matches")));
-	
-	// getMatch(CURRENTMATCHES, null)
-	// 	.then(results => matchesCallback(results, $("#current-results")));
-	const mockResults = [{
-		"away_team" : {
-			"country": "England",
-			"goals": 7
-		},
-		"home_team" : {
-			"country" : "Portugal",
-			"goals" : 5
-		},
-		"status": "future"
-	}];
-	matchesCallback(mockResults, $("#current-results"));
 
+	getMatch(CURRENTMATCHES, null)
+		.then(results => matchesCallback(results, $("#current-results")));
 
 	$("#login-form").show();
 	$("#register-form").hide();
@@ -96,12 +89,12 @@ $(function () {
 	$("#login-submit").on("click", function () {
 		const username = $("#username").val().trim();
 		const password = $("#password").val().trim();
-		$.get("/api/users/"+username, function(data){
+		$.get("/api/users/" + username, function (data) {
 			console.log(data)
-			if(err){
+			if (err) {
 				throw err;
 			}
-			else if (username === data.user_name && password === data.user_password){
+			else if (username === data.user_name && password === data.user_password) {
 				$("#welcomeUser").append("Welcome, " + username);
 			}
 			else {
@@ -109,10 +102,10 @@ $(function () {
 			}
 
 		})
-	
+
 	})
-	
-	$("#register-submit").on("click", function(){
+
+	$("#register-submit").on("click", function () {
 		const username = $("#user_name").val().trim();
 		const email = $("#email").val().trim();
 		const password = $("#pass_word").val().trim();
@@ -130,10 +123,10 @@ $(function () {
 			user_password: password
 		};
 		console.log(newUser)
-	
+
 		$.post("api/users/", newUser)
-			.then($.get("/api/users/"+username, function(res){
-				if(err){
+			.then($.get("/api/users/" + username, function (res) {
+				if (err) {
 					throw err;
 				}
 				else {
@@ -141,8 +134,8 @@ $(function () {
 					$("#welcomeUser").append("Welcome, " + res.user_name);
 				}
 			})
-			
-		)
+
+			)
 	});
 
 
